5b015b3088b5c74e9c931c6a3eed871b9d194e5e
davewrld <keithdav23@gmail.com>
2024-12-04 12:44:39 +0300

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ *  Prints string  argument in STDOUT  
+ *  @param {string} message to be printed
+*/
+function displayMessage(message) {
+    console.log(message);
+}
+
+module.exports = displayMessage;
